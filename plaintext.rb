35e3067f97d957c2a6e5c06bcf2e071caae784d8
Emily Le <emilyle265@gmail.com>
2017-01-30 19:03:08 -0800

    update description

--- plaintext.rb
+++ plaintext.rb
@@ -1,5 +1,11 @@
-# Write a function named “plaintext”, which takes a single parameter of a string encoded in this format. It should return the decoded word in string form. Demonstrate good coding practices (clear variable names, comments, etc).
-# You may assume that coded strings are always legally encoded with this system. You may also assume that the digit for the number of characters to skip will just be a single digit, not a multi-digit number (that is, you’ll never need to skip more than 9 characters)—but we welcome solutions that can handle multi-digit characters to skip.
+# Write a function named “plaintext”, which takes a single parameter of a string encoded in this format. 
+# It should return the decoded word in string form. Demonstrate good coding practices (clear variable 
+# names, comments, etc).
+
+# You may assume that coded strings are always legally encoded with this system. You may also assume 
+# that the digit for the number of characters to skip will just be a single digit, not a multi-digit 
+# number (that is, you’ll never need to skip more than 9 characters)—but we welcome solutions that can 
+# handle multi-digit characters to skip.
 
 def plaintext(code)
   each_char = code.split("")
